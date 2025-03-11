3bf8c0fe5b5754024c18e1e21c68a2d0110b92a9
crystal <66188059+kc-aquino@users.noreply.github.com>
2025-03-11 17:31:03 +0800

    Sticky Banner

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Image, Dimensions } from 'react-native';
 import MyFlightCard from '~/components/MyFlightCard';
 import TableArrival_Departure from '~/components/TableArrival_Departure';
 import { flightSchedule } from '~/sampleData';
 
+const { width } = Dimensions.get('window');
+
 export default function Screen() {
     return (
-        <View className='flex-1 w-full bg-secondary/30'>
-            <ScrollView contentContainerStyle={{ paddingHorizontal: 8 }} showsVerticalScrollIndicator={false}>
-                <View className='w-full max-w-screen  '>
+        <View className='flex-1 w-full bg-secondary/30 '>
+            <ScrollView contentContainerStyle={{ paddingHorizontal: 8, paddingBottom: 65 }} showsVerticalScrollIndicator={false}>
+                <View className='w-full max-w-screen'>
                     <MyFlightCard flightSchedule={flightSchedule} />
                     <TableArrival_Departure flightSchedule={flightSchedule} />
                 </View>
             </ScrollView>
+
+            {/* Sticky Banner */}
+            <View className='absolute bottom-0 w-full'>
+                <Image
+                    source={require('~/assets/images/ads/pluswidth_banner_ad_1.png')}
+                    style={{ width: width, height: width * 0.2 }}
+                    resizeMode='cover'
+                />
+            </View>
         </View>
     );
 }
